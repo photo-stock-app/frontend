19e88bce1c9206bcd290f5686e22a5eec83f09b8
alonecat <htchtc052@gmail.com>
2024-08-01 21:33:54 +0300

    workflow update with context

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
     test 3443
+     {{ graphqlUrl }}
   </div>
 </template>
+
+<script setup>
+const config = useRuntimeConfig();
+const graphqlUrl = config.public.graphqlUrl;
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      graphqlUrl: process.env.GRAPHQL_URL || 'default_value'
+    }
+  }
 })
