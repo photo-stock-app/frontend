be131fe153dce8211f3fa778ca115dec85d120f6
alonecat <htchtc052@gmail.com>
2024-08-02 18:22:34 +0300

    Расшифровываем секрет

--- server/routes/hello.ts
+++ server/routes/hello.ts
@@ -1,5 +1,25 @@
-export default defineEventHandler((event) => {
-    return {
-        hello: `graphql_url: ` + process.env.GRAPHQL_URL
+import { readFileSync } from 'fs';
+import { defineEventHandler } from 'h3';
+
+// Путь к файлу секрета
+const secretPath = '/run/secrets/graphql_url';
+
+// Функция для чтения секрета из файла
+const getGraphqlUrl = () => {
+    try {
+        // Читаем содержимое файла
+        const secret = readFileSync(secretPath, 'utf8');
+        return secret.trim(); // Удаляем лишние пробелы и переводы строк
+    } catch (error) {
+        console.error('Error reading secret file:', error);
+        return null;
     }
-})
+};
+
+export default defineEventHandler((event) => {
+    const graphqlUrl = getGraphqlUrl();
+
+    return {
+        hello: `graphql_url: ${graphqlUrl}`
+    };
+});
