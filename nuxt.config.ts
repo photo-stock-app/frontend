427a59a2318f4e453be6c252893846a1a7b9b60e
alonecat <htchtc052@gmail.com>
2024-08-10 00:27:26 +0300

    Envs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,8 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  ssr: false, // Временно отключаем SSR
   runtimeConfig: {
     public: {
-      graphqlUrl: process.env.GRAPHQL_URL
+      graphqlUrl: process.env.GRAPHQL_URL || 'https://default-url.com'
     }
   }
 })
